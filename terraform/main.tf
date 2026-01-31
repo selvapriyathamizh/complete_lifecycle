6c849e63f39be1d94742e3f23361047e1e936e60
selvapriyathamizh <selvapriyatamil2020@gmail.com>
2026-01-31 21:07:54 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,7 @@ resource "aws_security_group" "web_sg" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0a145236ee857b0c5"   # Amazon Linux 2
+  ami           = "ami-03ea746da1a2e36e7"   # Amazon Linux 2
   instance_type = "t2.micro"
   key_name      = var.key_name
   security_groups = [aws_security_group.web_sg.name]
