7dda5dacfaba5f1b57b8c3ab89cdc09bcd1ebb1e
selvapriyathamizh <selvapriyatamil2020@gmail.com>
2026-01-31 19:30:22 +0530

    created main.tf file

---
+++ terraform/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_security_group" "web_sg" {
+  name = "web-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0a145236ee857b0c5"   # Amazon Linux 2
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+  security_groups = [aws_security_group.web_sg.name]
+
+  tags = {
+    Name = "ci-cd-demo"
+  }
+}
